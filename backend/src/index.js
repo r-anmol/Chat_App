ea820759e8e00c17941faeee99cda114fe9052c3
Aarush Gupta <aarushgupta@Aarush1212.local>
2025-04-16 19:56:38 +0530

    Deployment Changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 
+import path from "path";
+
 import {connectDB} from "./lib/db.js"
 import {app , server} from "./lib/socket.js"
 import authRoutes from "./routes/auth.route.js";
@@ -11,6 +13,7 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve()
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,6 +24,14 @@ app.use(cors({
 
 app.use("/api/auth" , authRoutes)
 app.use("/api/messages" , messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+     app.use(express.static(path.join(__dirname , "../frontend/dist")));
+
+     app.get("*" , (req , res) => {
+        res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"));
+     })
+}
 
 server.listen(PORT , () => {
     console.log(`Server is running on Port${PORT}`);
