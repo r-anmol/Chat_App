f3e33e54dc05ff043deb5ebdf46c0710cc190298
Aarush Gupta <aarushgupta@Aarush1212.local>
2025-04-16 23:08:23 +0530

    FINAl

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -3,6 +3,7 @@ import Message from "../models/message.model.js";
 import cloudinary from "../lib/cloudinary.js"
 import { getRecieverSocketId } from "../lib/socket.js";
 import { io } from "../lib/socket.js";
+import mongoose from "mongoose";
 
 export const getUsersForSidebar = async(req , res) => {
     try {
